d07e974bfe2ea6e7bd2a02adbf34910d0678382c
Vedant Panchariya <vedantkp1510@gmail.com>
2025-10-02 21:04:15 +0530

    Project Done

--- nav.js
+++ nav.js
@@ -2,6 +2,7 @@ const hamburger = document.getElementById("hamburger");
 hamburger.addEventListener("click",()=>{
     const links = document.getElementById("links-container");   
     links.classList.toggle("menu");
+    hamburger.classList.toggle("clicked");
 });
 window.addEventListener('scroll',function(){
     const navbar = document.querySelector(".navbar");
@@ -9,8 +10,10 @@ window.addEventListener('scroll',function(){
     if(window.scrollY > 30){
         navbar.classList.add("scrolled");
         container.classList.add("scrolled");
+        hamburger.classList.add("scrolled");
     }else{
         navbar.classList.remove("scrolled");
         container.classList.remove("scrolled");
+        hamburger.classList.remove("scrolled");
     }
 });
