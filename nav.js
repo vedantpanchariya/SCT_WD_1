0284e5715c5d689c8d5ac8c03862c69e2a98e620
Vedant Panchariya <vedantkp1510@gmail.com>
2025-10-02 19:36:47 +0530

    Added some color switching to get a scrolling effect
    added reponsiveness and a menu icon

--- nav.js
+++ nav.js
@@ -1,11 +1,11 @@
 window.addEventListener('scroll',function(){
     const navbar = document.querySelector(".navbar");
-    
-    if(window.scrollY > 50){
+    const container = document.querySelector(".container");
+    if(window.scrollY > 10){
         navbar.classList.add("scrolled");
-        
+        container.classList.add("scrolled");
     }else{
         navbar.classList.remove("scrolled");
-        
+        container.classList.remove("scrolled");
     }
 });
